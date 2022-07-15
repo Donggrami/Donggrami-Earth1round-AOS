f1159535870a0e1121dc24f59cd8011935c2cdef
dobby <dobbidd0@gmail.com>
2022-07-15 17:47:27 +0900

    [Feat] signup to main activity

--- app/src/main/java/com/example/earth1round_aos/main/signup/SignUpTermsActivity.kt
+++ app/src/main/java/com/example/earth1round_aos/main/signup/SignUpTermsActivity.kt
@@ -1,5 +1,6 @@
 package com.example.earth1round_aos.main.signup
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
@@ -8,9 +9,15 @@ import com.example.earth1round_aos.databinding.ActivitySignupTermsBinding
 class SignUpTermsActivity: AppCompatActivity() {
     lateinit var binding: ActivitySignupTermsBinding
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         binding = ActivitySignupTermsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // 화면 넘어가기
+        binding.signupTermOkCv.setOnClickListener {
+            startActivity(Intent(this, SignUpCharNameActivity::class.java))
+            finish()
+        }
     }
 }
