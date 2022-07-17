875e7cdb7e46b3fe3b5ed3853043771fe1649781
dobby <dobbidd0@gmail.com>
2022-07-17 18:31:29 +0900

    [Feat] Signup Activity

--- app/src/main/java/com/example/earth1round_aos/main/login/LoginActivity.kt
+++ app/src/main/java/com/example/earth1round_aos/main/login/LoginActivity.kt
@@ -1,9 +1,12 @@
 package com.example.earth1round_aos.main.login
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.earth1round_aos.databinding.ActivityLoginBinding
+import com.example.earth1round_aos.main.MainActivity
+import com.example.earth1round_aos.main.signup.SignUpActivity
 
 class LoginActivity: AppCompatActivity() {
     lateinit var binding: ActivityLoginBinding
@@ -14,6 +17,36 @@ class LoginActivity: AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
+        // 카카오 로그인
+        binding.loginKakaoLoginCv.setOnClickListener {
+            kakao()
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
+
+
+        // 구글 로그인
+        binding.loginGoogleLoginCv.setOnClickListener {
+            google()
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
+
+
+        // 회원이 아닐 경우
+        binding.loginNotMemberYetTv.setOnClickListener {
+            startActivity(Intent(this,SignUpActivity::class.java))
+            finish()
+        }
+
+    }
+
+    private fun kakao(){
+
+    }
+
+    private fun google(){
 
     }
 }
--- app/src/main/java/com/example/earth1round_aos/main/signup/SignUpActivity.kt
+++ app/src/main/java/com/example/earth1round_aos/main/signup/SignUpActivity.kt
@@ -1,5 +1,6 @@
 package com.example.earth1round_aos.main.signup
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
@@ -12,5 +13,32 @@ class SignUpActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState, persistentState)
         binding = ActivitySignupBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.signupGoogleSignupCv.setOnClickListener {
+            gSignup()
+            startActivity(Intent(this, SignUpTermsActivity::class.java))
+            finish()
+        }
+
+        binding.signupKakaoSignupCv.setOnClickListener {
+            kSignup()
+            startActivity(Intent(this, SignUpTermsActivity::class.java))
+            finish()
+        }
+
+        binding.signupLookAroundTv.setOnClickListener {
+            startActivity(Intent(this, SignUpCharNameActivity::class.java))
+            finish()
+        }
+
+
+    }
+
+    private fun gSignup(){
+
+    }
+
+    private fun kSignup() {
+
     }
 }
