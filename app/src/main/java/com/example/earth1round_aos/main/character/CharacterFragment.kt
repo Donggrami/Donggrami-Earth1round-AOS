fab864617e57644399bec2e1913a4c1806bc0ab6
dobby <dobbidd0@gmail.com>
2022-07-21 21:41:00 +0900

    [Feat] test map success!!

--- app/src/main/java/com/example/earth1round_aos/main/character/CharacterFragment.kt
+++ app/src/main/java/com/example/earth1round_aos/main/character/CharacterFragment.kt
@@ -24,16 +24,16 @@ class CharacterFragment : Fragment() {
 
         binding = FragmentCharacterBinding.inflate(inflater, container, false)
 
-<<<<<<< HEAD
+
         //화면 넘어가기
-=======
+
         binding.characterHamburgerBtnIv.setOnClickListener {
 //            (context as MainActivity).supportFragmentManager.beginTransaction()
 //                .replace(R.id. main_frm, SettingFragment()).commitAllowingStateLoss()
             startActivity(Intent(requireActivity(), SettingActivity::class.java))
         }
 
->>>>>>> 28e68f0a651332c56973ef6bedff917976dc68ad
+
         binding.characterHomeTrophyIv.setOnClickListener {
             (context as MainActivity).supportFragmentManager.beginTransaction()
                 .replace(R.id.main_frm, CharacterPrizeFragment()).commitAllowingStateLoss()
