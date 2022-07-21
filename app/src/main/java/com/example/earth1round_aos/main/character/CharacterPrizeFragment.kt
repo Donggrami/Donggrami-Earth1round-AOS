bf1dad3a6afe648d341b3bc3f1276daaef8af7b1
Seoljingni <gina030212@gmail.com>
2022-07-21 21:48:50 +0900

    select recyclerview

--- app/src/main/java/com/example/earth1round_aos/main/character/CharacterPrizeFragment.kt
+++ app/src/main/java/com/example/earth1round_aos/main/character/CharacterPrizeFragment.kt
@@ -26,7 +26,7 @@ class CharacterPrizeFragment : Fragment() {
         binding = FragmentCharacterPrizeBinding.inflate(inflater, container, false)
 
         //화면 돌아가기
-        binding.characterPrizeBackIb.setOnClickListener {
+        binding.characterPrizeBackIv.setOnClickListener {
             (context as MainActivity).supportFragmentManager.beginTransaction()
                 .replace(R.id. main_frm, CharacterFragment()).commitAllowingStateLoss()
         }
--- app/src/main/java/com/example/earth1round_aos/main/character/CharacterSelectFragment.kt
+++ app/src/main/java/com/example/earth1round_aos/main/character/CharacterSelectFragment.kt
@@ -5,13 +5,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.earth1round_aos.R
 import com.example.earth1round_aos.databinding.FragmentCharacterSelectBinding
 import com.example.earth1round_aos.main.MainActivity
+import com.example.earth1round_aos.main.character.Prize
+import com.example.earth1round_aos.main.character.PrizeRVAdapter
+import com.example.earth1round_aos.main.character.Select
+import com.example.earth1round_aos.main.character.SelectRVAdapter
 
 class CharacterSelectFragment : Fragment() {
 
     lateinit var binding : FragmentCharacterSelectBinding
+    private var selectDatas = ArrayList<Select>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,10 +29,29 @@ class CharacterSelectFragment : Fragment() {
         binding = FragmentCharacterSelectBinding.inflate(inflater, container, false)
 
         //화면 돌아가기
-        binding.characterSelectBackIb.setOnClickListener {
+        binding.characterSelectBackIv.setOnClickListener {
             (context as MainActivity).supportFragmentManager.beginTransaction()
                 .replace(R.id. main_frm, CharacterFragment()).commitAllowingStateLoss()
         }
+
+        selectDatas.apply {
+            add(Select(R.drawable.img_character_character))
+            add(Select(R.drawable.img_character_character))
+            add(Select(R.drawable.img_character_character))
+            add(Select(R.drawable.img_character_character))
+            add(Select(R.drawable.img_character_character))
+            add(Select(R.drawable.img_character_character))
+            add(Select(R.drawable.img_character_character))
+            add(Select(R.drawable.img_character_character))
+            add(Select(R.drawable.img_character_character))
+            add(Select(R.drawable.img_character_character))
+
+
+        }
+
+        val selectRVAdapter = SelectRVAdapter(selectDatas)
+        binding.characterSelectCustomRv.adapter = selectRVAdapter
+        binding.characterSelectCustomRv.layoutManager = GridLayoutManager(context, 3)
 
         return binding.root
     }
